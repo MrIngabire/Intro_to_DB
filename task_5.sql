db46579ba8bc01d839d9e957b3a363bd840f14d2
MrIngabire <162576803+MrIngabire@users.noreply.github.com>
2024-07-28 23:17:22 +0200

    Update task_5.sql3

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,9 @@
 -- Use the database
 USE alx_book_store;
 
+-- Insert a single row into the Customers table
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
 -- Select and display the data from the Customers table to verify the insertion
 SELECT * FROM Customers WHERE customer_id = 1;
