1949ecde875afaa96d73f5c6d716c9218babc48d
MrIngabire <162576803+MrIngabire@users.noreply.github.com>
2024-07-28 23:04:15 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,16 @@
+-- Use the database
+USE alx_book_store;
 
+-- Query to get the full description of the books table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE() AND 
+    TABLE_NAME = 'Books';
